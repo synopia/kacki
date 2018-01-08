10d6671643119c24ec83bc0841b13b68094782a0
synopia <paul.fritsche@gmail.com>
2018-01-08 23:44:24 +0100

    small fixes

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Kacki
 
-Simple chatbot for [matrix.org](https://matrix.org/]).
+Simple chatbot for [matrix.org](https://matrix.org/]), inspired by [rbot](https://github.com/ruby-rbot/rbot).
+
+Uses the matrix rest API to communicate with server, kotlin coroutines.
+
+Project contains some experimental code using [deeplearning4j](https://github.com/deeplearning4j).
+
 
 ## About the project
 
--- src/main/kotlin/org/synopia/kacki/App.kt
+++ src/main/kotlin/org/synopia/kacki/App.kt
@@ -1,17 +1,21 @@
 package org.synopia.kacki
 
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
 import kotlinx.coroutines.experimental.async
 import org.synopia.kacki.plugins.BotsnackPlugin
 import org.synopia.kacki.plugins.ChuckNorrisPlugin
 import org.synopia.kacki.plugins.MarkovPlugin
 
 class App(args: ArgParser) {
+    val baseUrl by args
+            .storing("-c", "--client-api", help = "URL of matrix client rest API (default is official one)")
+            .default("https://matrix.org/_matrix/client/r0https://matrix.org/_matrix/client/r0")
     val user by args.storing("-u", "--user", help = "username to login")
     val password by args.storing("-p", "--password", help = "password to login")
 
     fun start() {
-        val config = KackiConfig("https://matrix.org/_matrix/client/r0", user, password)
+        val config = KackiConfig(baseUrl, user, password)
 
         val kacki = Kacki(config)
         kacki.plugins += BotsnackPlugin()
